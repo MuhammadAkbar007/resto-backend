afa42fe4274b82a970b1037d17ed27d57ba9811c
MuhammadAkbar007 <fotismenos007@mail.ru>
2025-01-27 11:45:14 +0500

    Bismillah 🥇

---
+++ src/main/java/uz/akbar/resto/RestoBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.akbar.resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/akbar/resto/RestoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.akbar.resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
